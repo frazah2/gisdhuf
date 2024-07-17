5495d224d0cc927b23dacab5a0b158450820c9de
frazah2 <tankionlinefraz@mail.com>
2024-07-17 12:32:58 +0400

    first commit

---
+++ js/tryyy.js
@@ -0,0 +1,15 @@
+let FirstName = "Mirza"
+let Name = "Fraz"
+let Surname = "Ahmad"
+let DOB = 2002
+let current_year = 2024
+let college = "BDUIHK"
+let pro_lang = "Java Script"
+let car = "Hummer"
+let country = "Pakistan"
+let hobby = "Games"
+let game = "MineCraft"
+
+console.log(`My name is ${FirstName} ${Name} ${Surname}. I am almost ${current_year-DOB} years old.`)    
+console.log(`I graduated from ${college}. I am Learning ${pro_lang} rn. My favorite car is ${car}.`)
+console.log(`I am from ${country}. My hobby is to play ${hobby}. Favorite game is ${game}.`)
